853e90f2b92456d2c29e71a0e53eb7c55453fcdd
Iguan04ka <Iguana_2004@mail.ru>
2025-04-28 12:02:05 +0400

    init commit

---
+++ src/main/java/ru/iguana/weatherservicespringboot/WeatherServiceSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ru.iguana.weatherservicespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherServiceSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherServiceSpringBootApplication.class, args);
+	}
+
+}
